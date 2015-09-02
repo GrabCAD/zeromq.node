c5edd4c696051cc87f8d296234f37ac1c44762d8
Ryan Howard <ryan.howard@makerbot.com>
2015-09-02 15:08:03 -0400

    add prebuilt linux version

---
+++ prebuilt/electron/linux/x64/Release/.deps/Release/obj.target/zmq/binding.o.d
@@ -0,0 +1,41 @@
+cmd_Release/obj.target/zmq/binding.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src -I/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include -I/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/zmq/binding.o.d.raw  -c -o Release/obj.target/zmq/binding.o ../binding.cc
+Release/obj.target/zmq/binding.o: ../binding.cc \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include/v8.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include/v8-version.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include/v8config.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_version.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_version.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_buffer.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/smalloc.h \
+ ../node_modules/nan/nan.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-errno.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-version.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-unix.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-threadpool.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-linux.h \
+ /home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_object_wrap.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h
+../binding.cc:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include/v8.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include/v8-version.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/v8/include/v8config.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_version.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_version.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_buffer.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/smalloc.h:
+../node_modules/nan/nan.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-errno.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-version.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-unix.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-threadpool.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/deps/uv/include/uv-linux.h:
+/home/ryan/branches/eagle/zeromq.node/node_modules/electron-rebuild/lib/headers/.node-gyp/0.25.3/src/node_object_wrap.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
