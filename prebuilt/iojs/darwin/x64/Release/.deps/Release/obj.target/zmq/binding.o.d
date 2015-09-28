0e69d2405d6a983fc1e09b621aa20614b3345bf1
Howard Mak <howard@grabcad.com>
2015-09-28 11:20:10 -0400

    Upgrade from ZeroMQ 4.0.5_2 -> 4.1.3
    4.1.3 is the latest version on Homebrew, so this will help new Mac
    setups.  Existing Mac setups will need to upgrade.

--- prebuilt/iojs/darwin/x64/Release/.deps/Release/obj.target/zmq/binding.o.d
+++ prebuilt/iojs/darwin/x64/Release/.deps/Release/obj.target/zmq/binding.o.d
@@ -1,4 +1,4 @@
-cmd_Release/obj.target/zmq/binding.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/howard/.node-gyp/1.8.2/src -I/Users/howard/.node-gyp/1.8.2/deps/uv/include -I/Users/howard/.node-gyp/1.8.2/deps/v8/include -I../node_modules/nan -I/usr/local/Cellar/zeromq/4.0.5_2/include -I/opt/local/include -I/usr/local/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/zmq/binding.o.d.raw  -c -o Release/obj.target/zmq/binding.o ../binding.cc
+cmd_Release/obj.target/zmq/binding.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/howard/.node-gyp/1.8.2/src -I/Users/howard/.node-gyp/1.8.2/deps/uv/include -I/Users/howard/.node-gyp/1.8.2/deps/v8/include -I../node_modules/nan -I/usr/local/Cellar/zeromq/4.1.3/include -I/opt/local/include -I/usr/local/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/zmq/binding.o.d.raw  -c -o Release/obj.target/zmq/binding.o ../binding.cc
 Release/obj.target/zmq/binding.o: ../binding.cc \
   /Users/howard/.node-gyp/1.8.2/deps/v8/include/v8.h \
   /Users/howard/.node-gyp/1.8.2/deps/v8/include/v8-version.h \
@@ -7,8 +7,7 @@ Release/obj.target/zmq/binding.o: ../binding.cc \
   /Users/howard/.node-gyp/1.8.2/src/node_version.h \
   /Users/howard/.node-gyp/1.8.2/src/node_buffer.h \
   /Users/howard/.node-gyp/1.8.2/src/smalloc.h \
-  /usr/local/Cellar/zeromq/4.0.5_2/include/zmq.h \
-  ../node_modules/nan/nan.h \
+  /usr/local/Cellar/zeromq/4.1.3/include/zmq.h ../node_modules/nan/nan.h \
   /Users/howard/.node-gyp/1.8.2/deps/uv/include/uv.h \
   /Users/howard/.node-gyp/1.8.2/deps/uv/include/uv-errno.h \
   /Users/howard/.node-gyp/1.8.2/deps/uv/include/uv-version.h \
@@ -26,7 +25,7 @@ Release/obj.target/zmq/binding.o: ../binding.cc \
 /Users/howard/.node-gyp/1.8.2/src/node_version.h:
 /Users/howard/.node-gyp/1.8.2/src/node_buffer.h:
 /Users/howard/.node-gyp/1.8.2/src/smalloc.h:
-/usr/local/Cellar/zeromq/4.0.5_2/include/zmq.h:
+/usr/local/Cellar/zeromq/4.1.3/include/zmq.h:
 ../node_modules/nan/nan.h:
 /Users/howard/.node-gyp/1.8.2/deps/uv/include/uv.h:
 /Users/howard/.node-gyp/1.8.2/deps/uv/include/uv-errno.h:
